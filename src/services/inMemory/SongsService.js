684790fb54c7a92a96f084508bae3847b8094fcd
nursyahrina <nursyahrina17@gmail.com>
2021-06-10 04:20:08 +0700

    create open-song API using Hapi plugin

---
+++ src/services/inMemory/SongsService.js
@@ -0,0 +1,91 @@
+const { nanoid } = require('nanoid');
+
+class SongsService {
+  constructor() {
+    this._songs = [];
+  }
+
+  addSong({
+    title,
+    year,
+    performer,
+    genre,
+    duration,
+  }) {
+    const id = `song-${nanoid(16)}`;
+    const insertedAt = new Date().toISOString();
+    const updatedAt = insertedAt;
+
+    const newSong = {
+      title,
+      year,
+      performer,
+      genre,
+      duration,
+      id,
+      insertedAt,
+      updatedAt,
+    };
+
+    this._songs.push(newSong);
+
+    const isSuccess = this._songs.filter((song) => song.id === id).length > 0;
+
+    if (!isSuccess) {
+      throw new Error('Lagu gagal ditambahkan');
+    }
+
+    return id;
+  }
+
+  getNotes() {
+    return this._songs;
+  }
+
+  getNoteById(id) {
+    const song = this._songs.filter((s) => s.id === id)[0];
+    if (!song) {
+      throw new Error('Lagu tidak ditemukan');
+    }
+
+    return song;
+  }
+
+  editNoteById(id, {
+    title,
+    year,
+    performer,
+    genre,
+    duration,
+  }) {
+    const index = this._songs.findIndex((song) => song.id === id);
+
+    if (index === -1) {
+      throw new Error('Gagal memperbarui lagu. Id tidak ditemukan');
+    }
+
+    const updatedAt = new Date().toISOString();
+
+    this._songs[index] = {
+      ...this._songs[index],
+      title,
+      year,
+      performer,
+      genre,
+      duration,
+      updatedAt,
+    };
+  }
+
+  deleteNoteById(id) {
+    const index = this._songs.findIndex((song) => song.id === id);
+
+    if (index === -1) {
+      throw new Error('Lagu gagal dihapus. Id tidak ditemukan');
+    }
+
+    this._songs.splice(index, 1);
+  }
+}
+
+module.exports = SongsService;
