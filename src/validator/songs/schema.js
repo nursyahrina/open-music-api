1ede7df4b81b82f62ff484a9f7503eeaab54dbe1
nursyahrina <nursyahrina17@gmail.com>
2021-06-11 00:19:48 +0700

    specify type to number:integer

--- src/validator/songs/schema.js
+++ src/validator/songs/schema.js
@@ -2,10 +2,10 @@ const Joi = require('joi');
 
 const SongPayloadSchema = Joi.object({
   title: Joi.string().required(),
-  year: Joi.number().required(),
+  year: Joi.number().required().integer(),
   performer: Joi.string().required(),
   genre: Joi.string(),
-  duration: Joi.number(),
+  duration: Joi.number().integer(),
 });
 
 module.exports = { SongPayloadSchema };
